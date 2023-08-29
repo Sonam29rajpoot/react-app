aea38659690bfa48dbfd0beef62bc216beb1e4e9
VarshaKumari7 <varsha.kumari@binaryinformatics.com>
2023-08-29 19:02:55 +0530

    valid name

--- src/components/Registration/registration.jsx
+++ src/components/Registration/registration.jsx
@@ -219,7 +219,12 @@ export default function Registration() {
             <div>
               <button
                 type="submit"
-                disabled={!isPhoneValid || !isEmailValid || !isPasswordValid}
+                disabled={
+                  !isValidName ||
+                  !isPhoneValid ||
+                  !isEmailValid ||
+                  !isPasswordValid
+                }
                 onClick={onSubmitHandler}
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
